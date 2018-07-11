da123fcffb8c28b14554960797a25523acbf8876
shripriyaprabhakar <37489016+shripriyaprabhakar@users.noreply.github.com>
2018-07-10 19:41:42 -0700

    Complete bare min req

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,78 @@
 
+$('document').ready(function() {
+  $("#dataform").submit(function(event) {
+    event.preventDefault();
+   console.log($('.textClass').val());
+    $.ajax({
+      type: "POST",
+      url: '/',
+      contentType: 'application/json', 
+      data: $('.textClass').val(),
+      success: function( data ) {
+        console.log(data);
+        $(".response").text(data);
+      }
+    });
 
+
+
+  });
+});
+
+
+
+// $(document).ready(function() {
+
+// 		// init: function () {
+// 		//   app.fetch();
+//     var data1;
+// 	$('#dataForm').on('click', function(e) {
+// 		    e.preventDefault();
+	
+// 		  //  app.handleSubmit();
+// 		  });
+		
+//     //var app = {
+// 		//send: function (json) {
+// 		    $.ajax({
+// 		      url: '/',
+// 		      type: 'POST',
+// 		      data1: $('.textClass').val(),
+		  
+// 		      data: JSON.stringify(data1),
+// 		      contentType: 'application/json',
+// 		      success: function (data) {
+//                   // fetch();
+//                   $('.response').append(data);
+// 		        console.log('Message sent to server');
+// 		      },
+// 		      error: function (data) {
+// 		        console.error('Failed to send message', data1);
+// 		      }
+// 		    });
+// 		//}
+
+// 		 // var fetch =  function (data) {
+// 	  //   	$.ajax({
+// 	  //      		url: 'http://127.0.0.1:3000/data',
+// 	  //     		type: 'GET',
+// 		 //        //data: JSON.stringify('data'),
+// 		 //        contentType: 'application/json',
+// 		 //        success: function (data) {
+// 		 //           $('.response').append(data);
+// 		 //        },
+// 		 //        error: function (data) {
+// 		 //        	console.error('Failed to send message', data1);
+// 		 //        }
+// 		 //    });
+// 	   // },
+
+// 		// handleSubmit: function (json) {
+// 	 //       app.send(json);
+// 	 //       console.log('submitted');
+// 	 //    }
+
+// 	//}
+//      //}
+
+// });
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,163 @@
+var express = require('express')
+var bodyParser = require('body-parser');
+var app = express()
+app.use(express.static('client'));
+
+var port = 3000;
+var ip = '127.0.0.1';
+
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
+app.post('/', function (req, res) {
+	console.log('req.body:', typeof req.body);
+
+  res.send(flattenData((req.body)));
+})
+
+ app.listen(port);
+ console.log('Listening on http://' + ip + ':' + port);
+
+var flattenData =  function(obj) {
+   let data = "";
+  let array = [];
+
+  for (let key in obj) {
+  	if (key !== "children") {
+   	  array.push(key);
+  	}
+  }
+  data += array.join(',');
+  data += "\n";
+
+  let recurse = function(object) {
+  	let arr = [];
+  	for (let key in object) {
+  	  if (key !== "children") {
+  	    arr.push(object[key]);
+  	  }
+  	}
+
+  	data += arr.join(',');
+  	data += "\n";
+
+  	for (let child of object.children) {
+  	  recurse(child);
+  	}
+  }
+
+  recurse(obj);
+
+  return data;
+
+   }
+
+
+
+/*
+
+var flattenData =  function(obj) {
+  var results = [];
+  var temp_arr = [];
+  var newObj = JSON.parse(JSON.stringify(obj));
+  delete newObj["children"];
+  //console.log(newObj);
+  temp_arr.push(newObj);
+  temp_arr.forEach(function(element){
+    var stringArr = [];
+    for (var key in element) {
+      stringArr.push(element[key]);
+    }
+     results.push(stringArr);
+  });
+ //console.log(results);
+ var newArr = [];
+ results.forEach(function(element){
+   //var temp = [];
+     newArr.push(element.join(','));
+      });
+ //console.log(newArr);
+         var string = '';
+      newArr.forEach(function(ele){
+           string = string + ele;
+          
+      })
+      
+      return string;
+
+   
+  if ((Array.isArray(obj["children"])) && (obj["children"].length > 0)) {
+    obj["children"].forEach(function(item){
+      var new_arr = flattenData(item);
+      temp_arr.concat(new_arr);
+      //console.log(temp_arr);
+    });
+  }
+  // else {
+  //   //console.log(temp_arr);
+  //   return temp_arr;
+    
+  // }
+ 
+}
+var obj = {
+    "firstName": "Joshie",
+    "lastName": "Wyattson",
+    "county": "San Mateo",
+    "city": "San Mateo",
+    "role": "Broker",
+    "sales": 1000000,
+    "children": [
+    {
+      "firstName": "Beth Jr.",
+      "lastName": "Johnson",
+      "county": "San Mateo",
+      "city": "Pacifica",
+      "role": "Manager",
+      "sales": 2900000,
+      "children": [
+        {
+          "firstName": "Smitty",
+          "lastName": "Won",
+          "county": "San Mateo",
+          "city": "Redwood City",
+          "role": "Sales Person",
+          "sales": 4800000,
+          "children": []
+        },
+        {
+          "firstName": "Allen",
+          "lastName": "Price",
+          "county": "San Mateo",
+          "city": "Burlingame",
+          "role": "Sales Person",
+          "sales": 2500000,
+          "children": []
+        }
+      ]
+    },
+    {
+      "firstName": "Beth",
+      "lastName": "Johnson",
+      "county": "San Francisco",
+      "city": "San Francisco",
+      "role": "Broker/Sales Person",
+      "sales": 7500000,
+      "children": []
+    }
+  ]
+};
+
+flattenData(obj);
+
+
+*/
