49af524d2e08f14e7663f357716fc793ee554839
shripriyaprabhakar <shripriyaprabhakar@gmail.com>
2018-07-11 21:37:46 -0700

    implement code in app.js

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -8,15 +8,47 @@ class App extends React.component {
   constructor (props) {
     super (props);
     this.state = {
-      customers = []
+      counter = 0;
     }
   }
   this.addCustomerInfo = this.addCustomerInfo.bind(this);
+  this.addAddressInfo = this.addAddressInfo.bind(this);
+  this.addAddressInfo = this.addAddressInfo.bind(this);
   }
 
-
-
-
+  render ()
+  if (this.state.counter === 0) {
+    return {
+        <div>
+          <form id="f1">
+            <input class="name" type="text" name = "Firstinputfield" placeholder="Your name">
+            <br>
+            <input class="email" type="text" name = "Firstinputfield" placeholder="Your e-mail">
+            <br>
+            <input class="password" type="text" name = "Firstinputfield" placeholder="password">
+            <input type="submit" value="next">
+          </form> 
+        </div> 
+    } else if (this.state.counter === 1) {
+          <form id="f1">
+            <input class="name" type="text" name = "Firstinputfield" placeholder="Your name">
+            <br>
+            <input class="email" type="text" name = "Firstinputfield" placeholder="Your e-mail">
+            <br>
+            <input class="password" type="text" name = "Firstinputfield" placeholder="password">
+            <input type="submit" value="next">
+          </form> 
+    } else if (this.state.counter === 1) {
+          <form id="f1">
+            <input class="name" type="text" name = "Firstinputfield" placeholder="Your name">
+            <br>
+            <input class="email" type="text" name = "Firstinputfield" placeholder="Your e-mail">
+            <br>
+            <input class="password" type="text" name = "Firstinputfield" placeholder="password">
+            <input type="submit" value="next">
+          </form> 
+   
+    }
 
 
 }
